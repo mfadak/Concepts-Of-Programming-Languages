0b73cc32670f2ad8b790889a28e5d05034927032
M. Fatih ADAK <62707529+mfadak@users.noreply.github.com>
2022-02-27 11:53:02 +0300

    Program.java

---
+++ Week2/Source4/src/Program.java
@@ -0,0 +1,10 @@
+public class Program
+{
+  public static void main(String[] args) {
+        ArrayList<ArrayList<Double>> liste = new ArrayList<>();
+        ArrayList<Double> eleman = new ArrayList<>();
+        eleman.add(85.78);
+        liste.add(eleman);
+        var tmp = liste.get(0);
+    }
+}
