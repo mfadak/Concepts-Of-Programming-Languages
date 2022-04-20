4894ad6d9a3d61252ee716f5bedc6b7de96ec992
M. Fatih ADAK <62707529+mfadak@users.noreply.github.com>
2022-04-20 22:44:57 +0300

    Test.java

---
+++ Week10/SoyutSınıfJava/src/pkt/Test.java
@@ -0,0 +1,15 @@
+package pkt;
+
+public class Test {
+
+	public static void main(String[] args) {
+		Daire d = new Daire(12,"Mavi");
+		System.out.println(d);
+		
+		System.out.println();
+		
+		Kare k = new Kare(5,"Siyah");
+		System.out.println(k);
+	}
+
+}
